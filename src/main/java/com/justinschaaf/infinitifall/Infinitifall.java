892f919bbc9c1e8b16bd50611834a18c93520bb7
MidnightTale <midnighttale24@gmail.com>
2023-04-29 23:49:05 +0700

    organize for future extend

--- src/main/java/com/justinschaaf/infinitifall/Infinitifall.java
+++ src/main/java/com/justinschaaf/infinitifall/Infinitifall.java
@@ -1,25 +1,18 @@
 package com.justinschaaf.infinitifall;
 
-import com.justinschaaf.infinitifall.util.HandleUtil;
 import com.justinschaaf.infinitifall.util.MiscUtil;
+import com.justinschaaf.infinitifall.watcher.EventWatcher;
 import io.papermc.paper.event.entity.EntityMoveEvent;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Infinitifall extends JavaPlugin implements Listener {
     public static Infinitifall instance;
     public static MiscUtil MiscUtil;
-    private double tpY = -64.0;
-    private double addY = 182.0;
-    private double damage = 4.0;
-    private boolean removeNonLiving = true;
-
+    public static EventWatcher EventWatcher;
     @Override
     public void onEnable() {
 
@@ -32,37 +25,12 @@ public final class Infinitifall extends JavaPlugin implements Listener {
 
     @EventHandler(priority = EventPriority.LOWEST)
     public void onEntityMove(EntityMoveEvent e) {
-        doCircumnavigation(e.getEntity());
+        EventWatcher.doCircumnavigation(e.getEntity());
     }
 
     @EventHandler(priority = EventPriority.LOWEST)
     public void onPlayerMove(PlayerMoveEvent e) {
-        doCircumnavigation(e.getPlayer());
+        EventWatcher.doCircumnavigation(e.getPlayer());
     }
 
-    private void doCircumnavigation(Entity e) {
-        if (e.getLocation().getY() < tpY) {
-            if (HandleUtil.isPaperFolia()) {
-
-                e.teleportAsync(e.getLocation().add(0, addY, 0));
-
-            } else {
-
-                e.teleport(e.getLocation().add(0, addY, 0));
-
-            }
-
-            EntityDamageEvent voidDamage = new EntityDamageEvent(e, EntityDamageEvent.DamageCause.VOID, damage);
-            getServer().getPluginManager().callEvent(voidDamage);
-
-            if (!voidDamage.isCancelled()) {
-
-                if (e instanceof LivingEntity) ((LivingEntity) e).damage(voidDamage.getDamage());
-                else if (removeNonLiving) e.remove();
-
-                e.setLastDamageCause(voidDamage);
-
-            }
-        }
-    }
 }
---
+++ src/main/java/com/justinschaaf/infinitifall/watcher/EventWatcher.java
@@ -0,0 +1,39 @@
+package com.justinschaaf.infinitifall.watcher;
+
+import com.justinschaaf.infinitifall.util.HandleUtil;
+import com.justinschaaf.infinitifall.util.MiscUtil;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.entity.EntityDamageEvent;
+
+import static com.justinschaaf.infinitifall.util.MiscUtil.damage;
+import static com.justinschaaf.infinitifall.util.MiscUtil.removeNonLiving;
+import static org.bukkit.Bukkit.getServer;
+
+public class EventWatcher {
+    public void doCircumnavigation(Entity e) {
+        if (e.getLocation().getY() < MiscUtil.tpY) {
+            if (HandleUtil.isPaperFolia()) {
+
+                e.teleportAsync(e.getLocation().add(0, MiscUtil.addY, 0));
+
+            } else {
+
+                e.teleport(e.getLocation().add(0, MiscUtil.addY, 0));
+
+            }
+
+            EntityDamageEvent voidDamage = new EntityDamageEvent(e, EntityDamageEvent.DamageCause.VOID, damage);
+            getServer().getPluginManager().callEvent(voidDamage);
+
+            if (!voidDamage.isCancelled()) {
+
+                if (e instanceof LivingEntity) ((LivingEntity) e).damage(voidDamage.getDamage());
+                else if (removeNonLiving) e.remove();
+
+                e.setLastDamageCause(voidDamage);
+
+            }
+        }
+    }
+}
