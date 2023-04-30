83a2b6e77aea74c1e94b1e133e4ffb2a645acb61
Justin Schaaf <justinhschaaf@outlook.com>
2023-04-29 20:47:08 -0700

    Reorganization and Refactoring
    - Refactored EventWatcher -> Circumnavigator and moved event listeners here
    - Refactored ReloadCommand -> CommandHandler and rewrote it to be more generic, supporting theoretical future subcommands
    - Refactored MiscUtil -> Config and added command messages
    - Refactored HandleUtil -> Util and made tryTeleportAsync search for the Entity#teleportAsync(Location) method rather than the TeleportCause class

---
+++ src/main/java/com/justinschaaf/infinitifall/CommandHandler.java
@@ -0,0 +1,56 @@
+package com.justinschaaf.infinitifall;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class CommandHandler implements CommandExecutor {
+
+    private Infinitifall plugin;
+
+    public CommandHandler(Infinitifall plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        boolean unknown = true;
+
+        if (args.length > 0) {
+
+            switch (args[0]) {
+
+                case "reload" -> {
+
+                    if (!sender.hasPermission("infinitifall.reload")) {
+                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', Config.msgErrorNoPerms));
+                        break;
+                    }
+
+                    try {
+                        Config.load(plugin);
+                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', Config.msgReloadConfirmed));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', Config.msgReloadErrored));
+                    }
+
+                    unknown = false;
+
+                }
+
+            }
+
+        } else unknown = true;
+
+        if (unknown) {
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', Config.msgErrorUnknownCmd));
+        }
+
+        return true;
+
+    }
+
+}
--- src/main/java/com/justinschaaf/infinitifall/Infinitifall.java
+++ src/main/java/com/justinschaaf/infinitifall/Infinitifall.java
@@ -1,41 +1,17 @@
 package com.justinschaaf.infinitifall;
 
-import com.justinschaaf.infinitifall.command.ReloadCommand;
-import com.justinschaaf.infinitifall.util.HandleUtil;
-import com.justinschaaf.infinitifall.util.MiscUtil;
-import com.justinschaaf.infinitifall.watcher.EventWatcher;
-import io.papermc.paper.event.entity.EntityMoveEvent;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Infinitifall extends JavaPlugin implements Listener {
-    public static Infinitifall instance;
+public final class Infinitifall extends JavaPlugin {
+
     @Override
     public void onEnable() {
 
-        instance = this;
-        saveDefaultConfig();
-        MiscUtil.reload();
+        Config.load(this);
 
-        getServer().getPluginManager().registerEvents(instance, instance);
-        getServer().getPluginManager().registerEvents(new EventWatcher(), instance);
-        getServer().getPluginManager().registerEvents(new MiscUtil(), instance);
-        getServer().getPluginManager().registerEvents(new HandleUtil(), instance);
-        instance.getCommand("infinitifallreload").setExecutor(new ReloadCommand());
+        getServer().getPluginManager().registerEvents(new Circumnavigator(), this);
+        getCommand("infinitifall").setExecutor(new CommandHandler(this));
 
-    }
-
-    @EventHandler(priority = EventPriority.LOWEST)
-    public void onEntityMove(EntityMoveEvent e) {
-        EventWatcher.doCircumnavigation(e.getEntity());
-    }
-
-    @EventHandler(priority = EventPriority.LOWEST)
-    public void onPlayerMove(PlayerMoveEvent e) {
-        EventWatcher.doCircumnavigation(e.getPlayer());
     }
 
 }
